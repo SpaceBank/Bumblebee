f78b1fc3338f3a6f4e8bed3f431c6ad8d330741c
Giorgi Martiashvili <Giorgi.Martiashvili@space.ge>
2021-06-01 00:41:01 +0400

    ref

--- Bumblebee/Bumblebee.cs
+++ Bumblebee/Bumblebee.cs
@@ -12,33 +12,46 @@ namespace Bumblebee
         {
             if (Variables.Step == "1")
             {
-                var data = await nodeServices.InvokeAsync<string>("transform", File.ReadAllText(Variables.SourceJsonPath), File.ReadAllText("./processes.json"), Variables.SourceStateDiagramId, Variables.DestinationStateDiagramId);
-
-                File.WriteAllText("Content.json", data);
-
-                Variables.Step = "2";
+                await TransformProcesses(nodeServices);
+                await GenerateProcessNames(nodeServices);
             }
 
             if (Variables.Step == "2")
             {
-                Console.WriteLine("Enter destination json path");
-                Variables.DestinationJsonPath = Console.ReadLine();
-
-                dynamic data = JsonConvert.DeserializeObject(await nodeServices.InvokeAsync<string>("process", File.ReadAllText(Variables.DestinationJsonPath), File.ReadAllText("./processes.json")));
-
-                File.WriteAllText("NewProcesses.json", JsonConvert.SerializeObject(data.processes, Formatting.Indented));
-                File.WriteAllText("ProcessNames.json", JsonConvert.SerializeObject(data.processNames, Formatting.Indented));
+                await GenerateProcessNames(nodeServices);
             }
 
             if (Variables.Step == "3")
             {
-                var processes = File.ReadAllText("./processes.json");
-
-                dynamic data = JsonConvert.DeserializeObject(await nodeServices.InvokeAsync<string>("newParameterInAllProcesses", File.ReadAllText(@"C:\Users\giorgi.martiashvili\Desktop\folder_15804_1620990248.json"), processes, "Traceparent"));
-                File.WriteAllText("NewJsonData.json", JsonConvert.SerializeObject(data, Formatting.Indented));
+                await GenerateProcessNames(nodeServices);
+                await AddNewParamToAll(nodeServices);
             }
 
             Console.WriteLine("Done");
+        }
+
+        public async Task TransformProcesses(INodeServices nodeServices)
+        {
+            var data = await nodeServices.InvokeAsync<string>("transform", File.ReadAllText(Variables.SourceJsonPath), File.ReadAllText("./processes.json"), Variables.SourceStateDiagramId, Variables.DestinationStateDiagramId);
+
+            File.WriteAllText("Content.json", data);
+        }
+
+        public async Task GenerateProcessNames(INodeServices nodeServices)
+        {
+            Console.WriteLine("Enter destination json path");
+            Variables.DestinationJsonPath = Console.ReadLine();
+
+            dynamic data = JsonConvert.DeserializeObject(await nodeServices.InvokeAsync<string>("process", File.ReadAllText(Variables.DestinationJsonPath), File.ReadAllText("./processes.json")));
+
+            File.WriteAllText("NewProcesses.json", JsonConvert.SerializeObject(data.processes, Formatting.Indented));
+            File.WriteAllText("ProcessNames.json", JsonConvert.SerializeObject(data.processNames, Formatting.Indented));
+        }
+
+        public async Task AddNewParamToAll(INodeServices nodeServices)
+        {
+            dynamic data = JsonConvert.DeserializeObject(await nodeServices.InvokeAsync<string>("newParameterInAllProcesses", File.ReadAllText(Variables.DestinationJsonPath), File.ReadAllText("NewProcesses.json"), "Traceparent"));
+            File.WriteAllText("NewJsonData.json", JsonConvert.SerializeObject(data, Formatting.Indented));
         }
     }
 }
